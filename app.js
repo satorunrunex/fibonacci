18352056e0c944fa7a18bd80df8ecb7f6c488220
satorunrunex <forthdimension@hotmail.co.jp>
2017-10-01 14:01:46 +0000

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 'use strict';
+function fib(n){
+    if(n===0){
+        return 0;
+    }else if(n===1){
+        return 1;
+    }
+    return fib(n-1) + fib(n-2);
+}
+const length = 40;
+for(let i = 0; i <= length; i++){
+    console.log(fib(i));
+}
+
+
+/* 自力で実装バージョン
+const f0 = 0;
+const f1 = 1;
+var fn = new Array(40);
+for ( let n = 0 ; n <=40 ; n++){
+    if(n === 0){
+        fn[0] = f0;        
+        console.log(fn[n] + ',');
+    }else if(n === 1){
+        fn[1] = f1;        
+        console.log(fn[n] + ',');
+    }else{
+        fn[n] = fn[n-1] + fn[n-2];             
+        console.log(fn[n] + ',');
+    }
+}
+*/
